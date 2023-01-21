199b53523c66a98b0381e424060f10e8912854c5
Kingdon Barrett <kingdon@weave.works>
2023-01-21 15:13:13 -0500

    github workflows with Docker and make
    python, mkdocs have to be added in a separate commit
    
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+###
+FROM ghcr.io/fluxcd/flux-cli:v0.38.3 AS flux
+
+###
+FROM nginx:1.23.2-alpine AS server
+
+COPY --from=flux /usr/local/bin/flux /usr/local/bin/flux
+
+RUN apk add rsync
+
+ADD _scripts/flux-pull.sh /usr/local/bin/
+RUN chmod +x /usr/local/bin/flux-pull.sh
---
+++ _scripts/flux-pull.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+set -x
+
+rm -rf /tmp/oci-pull
+mkdir /tmp/oci-pull
+
+flux pull artifact \
+  oci://img.hephy.pro/examples/howto-kubeconfig:testing \
+  --output /tmp/oci-pull && \
+  rsync --delete -rlv /tmp/oci-pull/ /usr/share/nginx/html
+
+if [ ! -f /usr/share/nginx/html/index.html ]
+then
+  echo "bailing, index.html wasn't present"
+  exit 1
+fi
+
+rm -rf /tmp/oci-pull
